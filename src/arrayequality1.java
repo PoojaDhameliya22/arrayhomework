463b84343c8e74116c61c0753d0ee4a440b280d9
poojadhameliya22 <dhameliyapooja45@gmail.com>
2022-05-11 20:09:14 +0100

    initial commit

---
+++ src/arrayequality1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class arrayequality1 {
+    public static void main(String[] args) {
+        int [] arr1 = new int [] {20, 5, -95, 43, 17};
+        int [] arr2 = new int [] {20, 5, -95, 43, 17};
+        System. out. println(Arrays. equals(arr1, arr2));
+    }
+}
